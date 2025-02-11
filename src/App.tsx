aca33fc668c1a4fd1f8ffe3b6c4a2cb961c6e6c7
mahdi <m.mazloum75@gmail.com>
2025-02-11 14:28:57 +0330

    feat: add axios dependency and custom fonts, update styling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ const App: React.FC = () => {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md space-y-4">
         <h1 className="text-2xl font-bold text-center">Hello, Tailwind!</h1>
-        <p className="text-gray-500 text-center">
+        <p className="text-gray-500 text-center ">
           This is a simple component styled with Tailwind CSS.
+          تست فونت
         </p>
-        <button className="w-full bg-red-500 text-white py-2 px-4 rounded hover:bg-blue-700">
-          Click Me
+        <button className="w-full bg-red-500 text-white py-2 px-4 rounded hover:bg-blue-700 ">
+          کلیک کنید
         </button>
       </div>
     </div>
